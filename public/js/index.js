0fc625a7abc50dc7e6b0bfdb60e68467bc642eb1
masterPiece93 <ankit8290@gmail.com>
2019-02-14 01:07:12 +0530

    =main code added

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var li = jQuery('<li></li>');
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
+  });
+});
